c86ddfeb9be7d8a6da8243d12845b46b038d0c11
dawson-lefebvre <112987023+dawson-lefebvre@users.noreply.github.com>
2024-03-03 04:29:28 -0700

    Created first crystal

---
+++ Assets/Crystal/Crystal.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            FindObjectOfType<PlayerManager>().Grow();
+            Destroy(gameObject);
+        }
+    }
+}
